7f3a666eabda68b3d5bb016b37f79cd15bbe7a09
anna-mat <anna.mathews05@gmail.com>
2024-10-03 22:39:17 -0400

    fixed lint issues

--- src/api/oh-queue.js
+++ src/api/oh-queue.js
@@ -1,18 +1,21 @@
+// Source: ChatGPT
+
 'use strict';
 
-const controllers = require.main.require('./src/controllers');
+// const controllers = require.main.require('./src/controllers');
 
-module.exports = function (app, middleware, controllers) {
-    // Define a route for the oh-queue page
-    app.get('/oh-queue', middleware.buildHeader, function (req, res) {
-        // Render the template for the oh-queue page
-        res.render('oh-queue', {});
-    });
+module.exports = function (app, middleware) {
+	// Define a route for the oh-queue page
+	app.get('/oh-queue', middleware.buildHeader, (req, res) => {
+		// Render the template for the oh-queue page
+		res.render('oh-queue', {});
+	});
 
-    app.get('/api/oh-queue', function (req, res) {
-        // Serve the data for the oh-queue page
-        res.json({
-            message: 'Welcome to the Office Hours Queue page!',
-        });
-    });
+	app.get('/api/oh-queue', (req, res) => {
+		// Serve the data for the oh-queue page
+		res.json({
+			message: 'Welcome to the Office Hours Queue page!',
+		});
+	});
 };
+
